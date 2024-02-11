95c751f796b968536373d1934bc9182f6baf070e
akshaykrishnap <akshaykrishnap071299@gmail.com>
2024-02-11 11:23:14 +0530

    Javascript

--- cal.js
+++ cal.js
@@ -1,7 +1,24 @@
 function display(num){
     console.log(typeof(num)); //let content= doucument.getelementbyid('result')
     //console.log(content);
-    result.value += num
+    result.value += num  // INPUT TAG ALWAYS RETURN AS STRING
     console.log(typeof(result.value));
   
+}
+function clearAll() {
+    result.value=""
+}
+function back(){
+    result.value=result.value.slice(0,-1)
+}
+function equal(){
+   try{
+    result.value=eval(result.value)
+   }
+catch (error){
+    result.value='error'
+    setTimeout(()=>{
+        result.value=""
+    },1000)
+}
 }
